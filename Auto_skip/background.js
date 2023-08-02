411b717294293de933f1ecae3147acceb83a576d
Mathiaszmrga <services@keyquest.tech>
2023-08-02 00:09:54 -0600

    adde auto skip with toggle

--- Auto_skip/background.js
+++ Auto_skip/background.js
@@ -1,26 +1,21 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.state !== undefined) {
-        chrome.storage.sync.set({ state: request.state }, function () {
-            if (request.state) {
-                chrome.tabs.query({ url: '*://*.youtube.com/*' }, function (tabs) {
-                    for (let tab of tabs) {
-                        chrome.scripting.executeScript({
-                            target: { tabId: tab.id },
-                            files: ['content.js']
-                        });
-                    }
-                });
-            }
-        });
+let youtubeState = false;
+let netflixState = false;
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        if (key === 'youtubeState') {
+            youtubeState = newValue;
+        }
+        if (key === 'netflixState') {
+            netflixState = newValue;
+        }
     }
 });
 
-chrome.storage.onChanged.addListener(function (changes, areaName) {
-    if (changes.state) {
-        chrome.tabs.query({ url: '*://*.youtube.com/*' }, function (tabs) {
-            for (let tab of tabs) {
-                chrome.tabs.sendMessage(tab.id, { state: changes.state.newValue });
-            }
-        });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'getYoutubeState') {
+        sendResponse({ state: youtubeState });
+    } else if (request.message === 'getNetflixState') {
+        sendResponse({ state: netflixState });
     }
 });
