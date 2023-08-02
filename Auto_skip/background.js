f4d5f0af1032d8b8efa51cf96572aa3261460655
Mathiaszmrga <services@keyquest.tech>
2023-08-02 01:04:43 -0600

    bugfix and revert to last working

--- Auto_skip/background.js
+++ Auto_skip/background.js
@@ -1,21 +1,4 @@
-let youtubeState = false;
-let netflixState = false;
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        if (key === 'youtubeState') {
-            youtubeState = newValue;
-        }
-        if (key === 'netflixState') {
-            netflixState = newValue;
-        }
-    }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'getYoutubeState') {
-        sendResponse({ state: youtubeState });
-    } else if (request.message === 'getNetflixState') {
-        sendResponse({ state: netflixState });
-    }
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ youtubeState: false });
+    chrome.storage.sync.set({ netflixState: false });
 });
