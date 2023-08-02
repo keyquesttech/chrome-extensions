c9ce9cd19a3bddf092ea6c44e595388bad278d1a
Mathiaszmrga <services@keyquest.tech>
2023-08-02 09:24:01 -0600

    Updated hbo and hbo bugfix

--- Auto_skip/content.js
+++ Auto_skip/content.js
@@ -2,7 +2,8 @@ setInterval(() => {
     chrome.storage.sync.get(['youtubeState'], function (data) {
         if (data.youtubeState) {
             let skipBtn = document.querySelector('.ytp-ad-skip-button.ytp-button') ||
-                Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Aa]d)/.test(el.textContent));
+                document.querySelector('.ytp-ad-overlay-close-button') ||
+                Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Aa]d|[Oo]mitir [Aa]nuncio)/.test(el.textContent));
             if (skipBtn) {
                 skipBtn.click();
             }
--- Auto_skip/netflixContent.js
+++ Auto_skip/netflixContent.js
@@ -2,7 +2,7 @@ setInterval(() => {
     chrome.storage.sync.get(['netflixState'], function (data) {
         if (data.netflixState) {
             let skipBtn = document.querySelector('button[data-uia="player-skip-intro"]') ||
-                Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Ii]ntro)/.test(el.textContent));
+                Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Ii]ntro|[Ss]altar [Ii]ntro)/.test(el.textContent));
             if (skipBtn) {
                 skipBtn.click();
             }
