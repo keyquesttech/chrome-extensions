3bb426cbcc7df0387833dc6106dfb66d3384866f
keyquest.tech <matias@keyquest.tech>
2023-11-03 11:36:24 -0600

    updated to persiste user settings

--- Auto_skip/content.js
+++ Auto_skip/content.js
@@ -1,11 +1,18 @@
 setInterval(() => {
-    chrome.storage.sync.get(['youtubeState'], function (data) {
+    // Use chrome.storage.local instead of chrome.storage.sync
+    chrome.storage.local.get(['youtubeState'], function (data) {
         if (data.youtubeState) {
-            let skipBtn = document.querySelector('.ytp-ad-skip-button.ytp-button') ||
-                document.querySelector('.ytp-ad-overlay-close-button') ||
-                Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Aa]d|[Oo]mitir [Aa]nuncio)/.test(el.textContent));
-            if (skipBtn) {
-                skipBtn.click();
+            try {
+                // Attempt to click the skip button if it exists
+                let skipBtn = document.querySelector('.ytp-ad-skip-button.ytp-button') ||
+                    document.querySelector('.ytp-ad-overlay-close-button') ||
+                    Array.from(document.querySelectorAll('button')).find(el => /(?:[Ss]kip [Aa]d|[Oo]mitir [Aa]nuncio)/.test(el.textContent));
+                if (skipBtn) {
+                    skipBtn.click();
+                }
+            } catch (error) {
+                // Log any errors to the console for troubleshooting
+                console.error('Error trying to skip ad:', error);
             }
         }
     });
--- Auto_skip/popup.js
+++ Auto_skip/popup.js
@@ -6,20 +6,25 @@ let youtubeStatus = document.getElementById('youtubeStatus');
 let netflixStatus = document.getElementById('netflixStatus');
 let hboStatus = document.getElementById('hboStatus');
 
-chrome.storage.sync.get(['youtubeState', 'netflixState', 'hboState'], function (data) {
+// Use chrome.storage.local to get the saved states
+chrome.storage.local.get(['youtubeState', 'netflixState', 'hboState'], function (data) {
     youtubeToggle.checked = data.youtubeState || false;
     netflixToggle.checked = data.netflixState || false;
     hboToggle.checked = data.hboState || false;
 });
 
+// Use chrome.storage.local to save the states
 youtubeToggle.addEventListener('change', function () {
-    chrome.storage.sync.set({ youtubeState: this.checked });
+    chrome.storage.local.set({ youtubeState: this.checked });
+    youtubeStatus.textContent = this.checked ? 'Enabled' : 'Disabled'; // Optional: Update status text
 });
 
 netflixToggle.addEventListener('change', function () {
-    chrome.storage.sync.set({ netflixState: this.checked });
+    chrome.storage.local.set({ netflixState: this.checked });
+    netflixStatus.textContent = this.checked ? 'Enabled' : 'Disabled'; // Optional: Update status text
 });
 
 hboToggle.addEventListener('change', function () {
-    chrome.storage.sync.set({ hboState: this.checked });
+    chrome.storage.local.set({ hboState: this.checked });
+    hboStatus.textContent = this.checked ? 'Enabled' : 'Disabled'; // Optional: Update status text
 });
